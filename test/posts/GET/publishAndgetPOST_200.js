7c9c8a401cdc9f8521296a3d233fe7c7bffc2892
Giovanni Arsenius <garsenius@odecee.com.au>
2016-11-30 11:12:12 +1100

    Refactored test and added a BeforeEach function to createPost first then Getting the Post by ID

--- test/posts/GET/publishAndgetPOST_200.js
+++ test/posts/GET/publishAndgetPOST_200.js
@@ -1,49 +1,42 @@
+'use strict';
+
+var hooks = require('../../hooks.js');
+
 describe('@get', function () {
+    let post;
 
-    //Using assert method
-    it('should return a 200 OK status code for PostID 1', function (done) {
-        utils.httpGET('/posts/1', {})
-            .set('content-type', 'application/json; charset=utf-8')
-            .expect(function (res) {
-                const response = res.body;
-                //console.log(response);
-                return assert.deepEqual(response,
-                    {
-                        "id": "1",
-                        "title": "foo",
-                        "body": "bar",
-                        "userId": 1
+    describe('Create POST', function () {
+
+        //Publish POST
+        beforeEach(function (done) {
+
+            hooks.createPost()
+                .then(function (_post) {
+                    post = _post;
+                    done();
+                });
+        });
+
+        //Get POST
+        describe('GET /posts/:id after Post Creation', function () {
+            //Using assert method to verify response returned with Post ID from createPost function
+            it('should return a 200 OK status code', function (done) {
+                utils.httpGET(`/posts/${post.id}`, {})
+                    .set('content-type', 'application/json; charset=utf-8')
+                    .expect(function (res) {
+                        const response = res.body;
+                        //show response return with Post ID
+                        console.log(response);
+                        expect(response).to.have.property('id');
+                        expect(response).to.have.property('title');
+                        expect(response).to.have.property('body');
+                        expect(response).to.have.property('userId');
+                        //Assert Post ID created is equal to the one return in response
+                        expect(response.id).to.equal(post.id);
                     })
-
-            })
-            .expect(200, done);
+                    .expect(200, done);
+            });
+        });
     });
-
-    //Using expect method
-    it('should return a 200 OK status code for PostID 1 using expect method', function (done) {
-        utils.httpGET('/posts/1', {})
-            .set('content-type', 'application/json; charset=utf-8')
-            .expect(function (res) {
-                const response = res.body;
-                //console.log(response);
-                expect(response).to.have.property('id');
-                expect(response).to.have.property('title');
-                expect(response).to.have.property('body');
-                expect(response).to.have.property('userId');
-                expect(response.id).to.equal('1');
-            })
-            .expect(200, done);
-    });
-
-    //Get All Posts
-    it('should return a 200 OK status code for PostID 1 using expect method', function (done) {
-        utils.httpGET('/posts/', {})
-            .set('content-type', 'application/json; charset=utf-8')
-            // .expect(function (res) {
-            //     const response = res.body;
-            //     console.log(response);
-            // })
-            .expect(200, done);
-    });
-
 });
+
