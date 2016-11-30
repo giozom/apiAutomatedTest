5094a5b36c9e3c3ab2a7a22afba32a630ec33a6a
Giovanni Arsenius <garsenius@odecee.com.au>
2016-11-30 11:10:45 +1100

    Updated hooks.js to add a createPost function

--- test/hooks.js
+++ test/hooks.js
@@ -1,1 +1,33 @@
-//Here you could have global hooks or database cleanup
+module.exports = {
+
+    createPost: function(){
+        return new Promise(function (resolve, reject){
+            const url = '/posts';
+            utils.httpPOST(url, {})
+                .send({
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1
+                })
+                .expect('content-type', 'application/json; charset=utf-8')
+                .expect(function (res) {
+                    const response = res.body;
+                    //console.log(response);
+                    expect(response.title).to.equal('foo');
+                    expect(response.body).to.equal('bar');
+                    expect(response.userId).to.equal(1);
+                    expect(response).to.have.property('id');
+
+                })
+                .expect(201)
+                .end(function (err,res) {
+
+                    if(err) {
+                        console.log(err);
+                        console.log(res.body)
+                    }
+                    resolve(res.body);
+                });
+        });
+    }
+};
---
+++ test/posts/GET/publishAndgetPOST_200.js
@@ -0,0 +1,49 @@
+describe('@get', function () {
+
+    //Using assert method
+    it('should return a 200 OK status code for PostID 1', function (done) {
+        utils.httpGET('/posts/1', {})
+            .set('content-type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                const response = res.body;
+                //console.log(response);
+                return assert.deepEqual(response,
+                    {
+                        "id": "1",
+                        "title": "foo",
+                        "body": "bar",
+                        "userId": 1
+                    })
+
+            })
+            .expect(200, done);
+    });
+
+    //Using expect method
+    it('should return a 200 OK status code for PostID 1 using expect method', function (done) {
+        utils.httpGET('/posts/1', {})
+            .set('content-type', 'application/json; charset=utf-8')
+            .expect(function (res) {
+                const response = res.body;
+                //console.log(response);
+                expect(response).to.have.property('id');
+                expect(response).to.have.property('title');
+                expect(response).to.have.property('body');
+                expect(response).to.have.property('userId');
+                expect(response.id).to.equal('1');
+            })
+            .expect(200, done);
+    });
+
+    //Get All Posts
+    it('should return a 200 OK status code for PostID 1 using expect method', function (done) {
+        utils.httpGET('/posts/', {})
+            .set('content-type', 'application/json; charset=utf-8')
+            // .expect(function (res) {
+            //     const response = res.body;
+            //     console.log(response);
+            // })
+            .expect(200, done);
+    });
+
+});
