e2824e54cc7d883da87e378f3f05004439caf070
Giovanni Arsenius <garsenius@odecee.com.au>
2016-11-30 11:12:57 +1100

    Refactored test and added a BeforeEach function to createPost first then Updating the Post by ID

--- test/posts/PUT/updatePOST_200.js
+++ test/posts/PUT/updatePOST_200.js
@@ -1,10 +1,27 @@
+'use strict';
+
+var hooks = require('../../hooks.js');
+
 describe('@put', function () {
 
-     it('should return a 200 code after updating postId 1', function (done) {
-        utils.httpPUT('/posts/20', {})
+let post;
+
+    //Publish POST
+    beforeEach(function (done) {
+
+        hooks.createPost()
+            .then(function (_post) {
+                post = _post;
+                done();
+            });
+    });
+
+    //Update POST
+    it('should return a 200 code after updating post', function (done) {
+        utils.httpPUT(`/posts/${post.id}`, {})
             .send(
                 {
-                    id: 20,
+                    id: post.id,
                     title: 'candy crush',
                     body: 'sodalicious juicy',
                     userId: 1
@@ -14,7 +31,7 @@ describe('@put', function () {
             .expect(function (res) {
                 const response = res.body;
                 //console.log(response);
-                expect(response.id).to.equal('20');
+                expect(response.id).to.equal(post.id);
                 expect(response.title).to.equal('candy crush');
                 expect(response.body).to.equal('sodalicious juicy');
                 expect(response.userId).to.equal(1);
@@ -23,30 +40,5 @@ describe('@put', function () {
             .expect(200, done);
 
     });
-
-    it('should return a 200 code after updating postId 1', function (done) {
-        utils.httpPUT('/posts/1', {})
-            .send(
-                {
-                    id: 1,
-                    title: 'foo',
-                    body: 'bar',
-                    userId: 1
-                }
-            )
-            .expect('content-type', 'application/json; charset=utf-8')
-            .expect(function (res) {
-                const response = res.body;
-                //console.log(response);
-                expect(response.id).to.equal('1');
-                expect(response.title).to.equal('foo');
-                expect(response.body).to.equal('bar');
-                expect(response.userId).to.equal(1);
-
-            })
-            .expect(200, done);
-
-    });
-
 
 });
